05c57dd9ab5159610f7db708486919990fceb468
Mhd. Zulfikar Pinem <ijuldev69@gmail.com>
2021-06-14 22:06:48 +0700

    :pencil: Upload files

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', () => {
+    let mmrAwal = document.getElementById('mmrAwal').value
+    let mmrTarget = document.getElementById('mmrTarget').value
+    const hasil = document.querySelector('.result')
+
+    let sisaMmr = mmrTarget - mmrAwal
+    let result
+
+    if (mmrAwal > mmrTarget) {
+        alert('Jangan diturunin dong MMR nya :(')
+        location.reload()
+    }
+    document.querySelector('.mmr-target').textContent = mmrTarget
+
+    if (mmrAwal < 2000) {
+        if(mmrTarget >= 3500) {
+            var total1 = (mmrTarget - 3499) / 1
+            let krg = mmrTarget - 3499
+            mmrTarget -= krg
+            if(mmrTarget >= 2000 && mmrTarget < 3500) {
+                var total2 = (mmrTarget - 1999) / 7
+                krg = mmrTarget - 1999
+                mmrTarget -= krg
+                if(mmrTarget < 2000) {
+                    sisaMmr = mmrTarget - mmrAwal
+                }
+            }
+            result = total1 + total2 + (sisaMmr / 20)
+        } else if (mmrTarget >= 2000 && mmrTarget < 3500) {
+            var total1 = (mmrTarget - 1999) / 7
+            let krg = mmrTarget - 1999
+            mmrTarget -= krg
+            if(mmrTarget < 2000) {
+                sisaMmr = mmrTarget - mmrAwal
+            }
+            result = total1 + (sisaMmr / 20)
+        } else {
+            result = sisaMmr / 20
+        }
+    } else if (mmrAwal >= 2000 && mmrAwal < 3500) {
+        if(mmrTarget >= 3500) {
+            var total1 = (mmrTarget - 3499) / 1
+            let krg = mmrTarget - 3499
+            mmrTarget -= krg
+            if(mmrTarget >= 2000 && mmrTarget < 3500) {
+                sisaMmr = mmrTarget - mmrAwal
+            }
+            result = total1 + (sisaMmr / 7)
+        } else {
+            result = sisaMmr / 7
+        }
+    } else if (mmrAwal >= 3500) {
+        result = sisaMmr / 1
+    }
+    
+    hasil.textContent = Math.ceil(result)
+    
+})
